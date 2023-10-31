d9d171559aa0214595b5321f75ca947a88a03801
Nikolay Grinko <ngrink.io@gmail.com>
2023-10-31 19:02:57 +0300

    chore(server): add postgres connection via typeorm

--- services/server/src/app.module.ts
+++ services/server/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ConfigOptions } from './config/dotenv.config';
+import { TypeOrmOptions } from './config/typeorm.config';
 
 @Module({
-  imports: [ConfigModule.forRoot(ConfigOptions)],
+  imports: [
+    ConfigModule.forRoot(ConfigOptions),
+    TypeOrmModule.forRootAsync(TypeOrmOptions),
+  ],
 })
 export class AppModule {}
