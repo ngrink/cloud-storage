93bd454bc693b0de87cb156785e2d7b303dd531d
Nikolay Grinko <ngrink.io@gmail.com>
2023-10-31 14:56:25 +0300

    chore(server): configure nest app
    - cli source root
    - api prefix
    - versioning
    - cors
    - validation pipe

--- services/server/src/main.ts
+++ services/server/src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = 7000;
+
+  app.setGlobalPrefix('api');
+  app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
+  app.enableCors({ credentials: true, origin: true });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
