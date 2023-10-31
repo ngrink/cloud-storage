5b19c769df12f5721861b3a51f8620792fe7acb5
Nikolay Grinko <ngrink.io@gmail.com>
2023-10-31 15:01:32 +0300

    chore(server): add swagger
    - add swagger module
    - setup nest-cli swagger plugin

--- services/server/src/main.ts
+++ services/server/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
@@ -17,6 +18,15 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Cloud Storage App')
+    .setDescription('API specification')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/docs/api', app, document);
 
   await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
