75c6f58fbd2d187d6a03dd8dda26624e784f007e
Nikolay Grinko <ngrink.io@gmail.com>
2023-11-01 12:28:32 +0300

    chore(server): add caching module with redis store

--- services/server/src/app.module.ts
+++ services/server/src/app.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CacheModule } from '@nestjs/cache-manager';
 
 import { ConfigOptions } from './config/dotenv.config';
 import { TypeOrmOptions } from './config/typeorm.config';
+import { CacheOptions } from './config/cache.config';
 import { AccountsModule } from './modules/accounts/accounts.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(ConfigOptions),
     TypeOrmModule.forRootAsync(TypeOrmOptions),
+    CacheModule.registerAsync(CacheOptions),
     AccountsModule,
   ],
 })
