38f932faf6a14ef27c4d4fbd72647188be378209
Nikolay Grinko <ngrink.io@gmail.com>
2023-11-01 14:37:59 +0300

    perf(server): add caching for accounts module

--- services/server/src/modules/accounts/accounts.repository.ts
+++ services/server/src/modules/accounts/accounts.repository.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 import { Account } from './entities/account.entity';
 import { Profile } from './entities/profile.entity';
@@ -15,6 +17,8 @@ export class AccountsRepository {
     private readonly accountModel: Repository<Account>,
     @InjectRepository(Profile)
     private readonly profileModel: Repository<Profile>,
+    @Inject(CACHE_MANAGER)
+    private readonly cacheManager: Cache,
   ) {}
 
   async createAccount(data: CreateAccountDto) {
@@ -25,34 +29,64 @@ export class AccountsRepository {
       ...data,
       profile: profile,
     });
-    await this.accountModel.save(account);
+    const accountDto = await this.accountModel
+      .save(account)
+      .then((account) => new AccountDto(account));
 
-    return new AccountDto(account);
+    await Promise.all([
+      this.cacheManager.set(`accounts:${account.id}`, accountDto),
+      this.cacheManager.set(`accounts:${account.email}`, accountDto),
+    ]);
+
+    return accountDto;
   }
 
   async getAccounts() {
+    const cachedAccounts = await this.cacheManager.get('accounts');
+    if (cachedAccounts) {
+      return cachedAccounts;
+    }
+
     const result = await this.accountModel.find();
     const accounts = result.map((account) => new AccountDto(account));
+
+    await this.cacheManager.set('accounts', accounts);
 
     return accounts;
   }
 
   async getAccount(accountId: number) {
+    const cachedAccount = await this.cacheManager.get(`accounts:${accountId}`);
+    if (cachedAccount) {
+      return cachedAccount;
+    }
+
     const account = await this.accountModel.findOneBy({ id: accountId });
     if (!account) {
       return null;
     }
 
-    return new AccountDto(account);
+    const accountDto = new AccountDto(account);
+    await this.cacheManager.set(`accounts:${accountId}`, accountDto);
+
+    return accountDto;
   }
 
   async getAccountByEmail(email: string) {
+    const cachedAccount = await this.cacheManager.get(`accounts:${email}`);
+    if (cachedAccount) {
+      return cachedAccount;
+    }
+
     const account = await this.accountModel.findOneBy({ email });
     if (!account) {
       return null;
     }
 
-    return new AccountDto(account);
+    const accountDto = new AccountDto(account);
+    await this.cacheManager.set(`accounts:${email}`, accountDto);
+
+    return accountDto;
   }
 
   async updateProfile(accountId: number, profile: UpdateProfileDto) {
@@ -63,9 +97,11 @@ export class AccountsRepository {
 
     Object.assign(account, { profile });
     await account.save();
+    await this.cacheManager.del(`accounts:${accountId}`);
   }
 
   async deleteAccount(accountId: number) {
     await this.accountModel.softDelete({ id: accountId });
+    await this.cacheManager.del(`accounts:${accountId}`);
   }
 }
